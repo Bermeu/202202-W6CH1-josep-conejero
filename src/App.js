9f28a006713910d3d4e121e74fa8bceb2deb7cbd
Josep Conejero <bermeu@gmail.com>
2022-02-15 21:15:14 +0100

    Added dispatch to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
+  const dispatch = useDispatch();
+  const todosTasks = useSelector((state) => {
+    return state.todosTasks;
+  });
   return <div className="TodosContainer"></div>;
 }
 
