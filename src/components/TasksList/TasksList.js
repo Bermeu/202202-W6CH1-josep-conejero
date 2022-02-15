65c72f8f079fc0f187a1c9bcb445909b5c08166c
Josep Conejero <bermeu@gmail.com>
2022-02-15 22:27:22 +0100

    Commented TasksList.js

--- src/components/TasksList/TasksList.js
+++ src/components/TasksList/TasksList.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+/* import PropTypes from "prop-types";
 import TaskItem from "../TaskItem/TaskItem";
 
 const TasksList = ({ className, tasksList }) => {
@@ -16,3 +16,4 @@ TasksList.propTypes = {
 };
 
 export default TasksList;
+ */
